a1915180bd2b8b39ac03380e50c04e2d6853f78f
isamposebe <nikita.sokol1999@mail.ru>
2021-10-25 10:25:05 +0300

    Working with a class Employee
    Добавил пару кометов(Поясните кто писал болевую переменную )
    Добавил пустой Print Думаю использовать его

--- EmployeeSelectionConsole/Employee.cs
+++ EmployeeSelectionConsole/Employee.cs
@@ -3,17 +3,32 @@
     public class Employee
     {
         private static readonly int mustHaveScore = 300;
-
+        /// <summary>
+        /// Полное имя
+        /// </summary>
         public string FullName { get; internal set; }
+        /// <summary>
+        /// Возрост(в годах)
+        /// </summary>
         public int Age { get; internal set; }
 
         internal GoodQualities GoodQualities { get; set; }
         internal BadQualities BadQualities { get; set; }
         internal GeneralSkills GeneralSkills { get; set; }
         internal SpecificSkills SpecificSkills { get; set; }
-
+        /// <summary>
+        /// Проверка чего-то?(Кто писал поясни)
+        /// </summary>
         public bool IsGoodEnough => ((int)SpecificSkills +
                                      (int)GeneralSkills -
                                      ((int)BadQualities)) >= mustHaveScore;
+        /// <summary>
+        /// Вывод основных хоректеристик(Еще в разработке возможно не будет использоватся)
+        /// </summary>
+        public void Print()
+        {
+            
+                
+        }
     }
 }
